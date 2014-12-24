b46392c687c50de08ecc5f6a9c0163e5e660c393
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2014-12-24 12:32:44 +0900

    メールをimapで取得して、slackにpostする部分までできた

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ imapで接続してメールを取得するアカウントのID・PWを入力し
 ```
 export EMAIL_ACCOUNT={あなたのemailアカウント}
 export EMAIL_PASSWORD={あなたのemailパスワード}
+export SLACK_DOMAIN={slackのチーム名}
+export SLACK_TOKEN={slackのトークン}
+export SLACK_CHANNEL={slackのpostしたいチャンネル}
 ```
 
 gmailだとbotとして弾かれたので簡易的にセキュリティレベルを下げて実行しました
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var inbox = require('inbox')
     Iconv = require('iconv').Iconv,
-    Mailparser = require('mailparser').MailParser;
+    Mailparser = require('mailparser').MailParser,
+    Slack = require('node-slack');
 
 var client = inbox.createConnection(false, "imap.gmail.com", {
   secureConnection: true,
@@ -9,8 +10,8 @@ var client = inbox.createConnection(false, "imap.gmail.com", {
       pass: process.env.EMAIL_PASSWORD
     }
 });
-
 var mailparser = new Mailparser();
+var slack = new Slack(process.env.SLACK_DOMAIN, process.env.SLACK_TOKEN);
 
 client.on("connect", function() {
   client.openMailbox("INBOX", function(error, info) {
@@ -20,17 +21,21 @@ client.on("connect", function() {
 });
 
 mailparser.on('end', function(email) {
-    console.log("from", email.from);
-    console.log("subject", email.subject);
-    console.log("text", email.text);
+    slack.send({
+        text: email.text,
+        channel: process.env.SLACK_CHANNEL,
+        username: 'Bot',
+        icon_emoji: 'test'
+    });
 });
 
+
 client.on("new", function(message) {
-  client.createMessageStream(message.UID).on("data", function(data){
-      var conv = new Iconv('UTF-8', 'ISO-8859-1');
-      var body = conv.convert(data).toString();
-      mailparser.write(body);
-      mailparser.end();
+    client.createMessageStream(message.UID).on("data", function(data){
+         var conv = new Iconv('UTF-8', 'ISO-8859-1');
+         var body = conv.convert(data).toString();
+         mailparser.write(body);
+         mailparser.end();
     });
 });
 
